bcfb4c63901717431961a3a501975efe82edd305
Kemji <kurtandrieasoy17@gmail.com>
2026-07-13 18:26:47 +0800

    Fix MinecraftDownloader: let offline accounts download game when online

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
@@ -87,30 +87,30 @@ public class MinecraftDownloader {
         sExecutorService.execute(() -> {
             try {
                 if(isLocalProfile || !isOnline) {
-                    String versionMessage = realVersion; // Use provided version unless we find its a modded instance
+                    String versionMessage = realVersion;
 
-                    // See if provided version is a modded version and if that version depends on another jar, check for presence of both jar's .json.
                     try {
-                        // This reads the .json associated with the provided version. If it fails, we can assume it's not installed.
                         File providedJsonFile = new File(Tools.DIR_HOME_VERSION + "/" + realVersion + "/" + realVersion + ".json");
                         JMinecraftVersionList.Version providedJson = Tools.GLOBAL_GSON.fromJson(Tools.read(providedJsonFile.getAbsolutePath()), JMinecraftVersionList.Version.class);
 
-                        // This checks if running modded version that depends on other jars, so we use that for the error message.
                         File vanillaJsonFile = new File(Tools.DIR_HOME_VERSION + "/" + providedJson.inheritsFrom + "/" + providedJson.inheritsFrom + ".json");
                         versionMessage = providedJson.inheritsFrom != null ? providedJson.inheritsFrom : versionMessage;
 
-                        // Ensure they're both not some 0 byte corrupted json
                         if (providedJsonFile.length() == 0 || vanillaJsonFile.exists() && vanillaJsonFile.length() == 0){
                             throw new RuntimeException("Minecraft "+versionMessage+ " is needed by " +realVersion); }
 
                         listener.onDownloadDone();
                     } catch (Exception e) {
-                        String tryagain = "Please ensure you have an internet connection";
-                        Tools.showErrorRemote(versionMessage + " is not currently installed. "+ tryagain, e);
+                        if (isOnline) {
+                            downloadGame(activity, version, realVersion);
+                            listener.onDownloadDone();
+                        } else {
+                            Tools.showErrorRemote(versionMessage + " is not currently installed. Please ensure you have an internet connection", e);
+                        }
                     }
-                }else {
-                downloadGame(activity, version, realVersion);
-                listener.onDownloadDone();
+                } else {
+                    downloadGame(activity, version, realVersion);
+                    listener.onDownloadDone();
                 }
             }catch (Exception e) {
                 listener.onDownloadFailed(e);
